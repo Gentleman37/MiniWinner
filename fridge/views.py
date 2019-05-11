483d10f69adf62d1626c4a44accf7f3eb354cf4e
NamJaeyong <reosd95@gmail.com>
2019-05-11 17:10:18 +0900

    nam

--- fridge/views.py
+++ fridge/views.py
@@ -35,7 +35,7 @@ def delete(request, post_pk):
     post.delete()
     return redirect('home')
 
-def about(request)
+def about(request):
     return render(request, 'about.html')
     
 def signup(request):
