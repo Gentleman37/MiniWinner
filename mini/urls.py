87e9f72440ec1a7761443783c23a47e2718a9d95
se yeon <kimse2878@korea.ac.kr>
2019-05-11 15:40:36 +0900

    fd

--- mini/urls.py
+++ mini/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from fridge import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('djagno.contrib.auth.urls')),
     path('accounts/signup', view.signup, name='signup'),
+    path('recipe/', views.recipe, name = 'recipe'), 
+    path('about/', name = 'about')
 ]
