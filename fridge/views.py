0263505345197903e98224c6cc0f6188204a9fa9
NamJaeyong <reosd95@gmail.com>
2019-05-12 03:11:40 +0900

    repairsignup

--- fridge/views.py
+++ fridge/views.py
@@ -92,7 +92,7 @@ def signup(request):
         if form.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
             auth.login(request, new_user)
-            return redirect('')
+            return redirect('home')
         else:
             form = UserForm()
             error = "아이디가 이미 존재합니다."
