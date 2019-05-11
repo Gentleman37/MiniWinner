ba452945f187a0ebbbf96aaf6981e3143ad080fa
Mira <miramira227@gmail.com>
2019-05-11 13:34:43 +0900

    base+new

--- fridge/models.py
+++ fridge/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=200)
+    exp_date = models.DateTimeField(max_length=200)
+    img = models.FileField(null=True)
+    family_choices=(
+        ("과일", '과일'),
+        ("야채", '야채'),
+        ("유제품", '유제품'),
+        ("육류", '육류'),
+        ("어류", '어류'),
+    )
+    family = models.CharField(
+        choices = family_choices,
+        default = "과일",
+        max_length=200,
+    )
+
+    def __str__(self):
+        return self.name
+
--- fridge/views.py
+++ fridge/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Post 
 
 # Create your views here.
+
+def new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        post = form.save(commit=False)
+        form.save()
+        return redirect('detail', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'new.html', { 'form' : form })
