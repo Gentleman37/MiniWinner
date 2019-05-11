5ef958d42d9b4a7ed4a252f50c64112fa5467ca2
NamJaeyong <reosd95@gmail.com>
2019-05-11 14:21:41 +0900

    signup&login

--- fridge/forms.py
+++ fridge/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('name', 'family', 'exp_date', 'img',)
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        widgets = {'password': forms.PasswordInput()}
+
+
--- fridge/views.py
+++ fridge/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm
+from .forms import PostForm, UserForm
 from .models import Post 
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -13,3 +15,20 @@ def new(request):
     else:
         form = PostForm()
     return render(request, 'new.html', { 'form' : form })
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            auth.login(request, new_user)
+            return redirect('')
+        else:
+            form = UserForm()
+            error = "아이디가 이미 존재합니다."
+            return render(request, 'registration/signup.html', {'form': form, 'error': error} )
+    else:
+        form = UserForm()
+        return render(request, 'registration/signup.html', {'form': form} )
